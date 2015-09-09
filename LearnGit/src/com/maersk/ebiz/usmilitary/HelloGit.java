fe752bed5cace4e85961d6649882610b7cb408c7
ashokpappu <ashokpappu@gmail.com>
2015-09-08 23:13:30 -0400

    This is the firstCommit

---
+++ LearnGit/src/com/maersk/ebiz/usmilitary/HelloGit.java
@@ -0,0 +1,10 @@
+package com.maersk.ebiz.usmilitary;
+
+/**
+ * Created by apappu_parental on 9/8/15.
+ */
+public class HelloGit {
+    public static void main(String args[]) {
+        System.out.println("Hello Git");
+    }
+}
